d131859555a5ef166d7229b8bed1094dc19be2b0
Michael <michaelmartinez4320@yahoo.com>
2024-06-06 20:52:29 -0400

    finish calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -10,16 +10,27 @@ const sum = function(arr) {
 	return arr.reduce((total, currentItem) => total + currentItem, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, currentItem) => total * currentItem);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let total = 1;
+	for (let i = 0; i < num2; i++) {
+    total = total * num1;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fact = 1;
+  let total = 1;
+  for (let i = 0; i < num; i++) {
+    total = total * fact;
+    fact++;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
